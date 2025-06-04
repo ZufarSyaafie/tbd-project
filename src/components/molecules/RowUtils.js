d63d3fe0fbb822317103772f71c8dc350f8f6bfa
haidar <haidar.syaafie@gmail.com>
2025-06-04 18:55:33 +0700

    Fixing Icons

--- src/components/molecules/RowUtils.js
+++ src/components/molecules/RowUtils.js
@@ -4,9 +4,15 @@ import React from 'react';
 export default function RowUtils() {
 	return (
 		<div className="flex space-x-[10px]">
-			<RectButton className={'bg-[#4285F4] text-xl font-bold text-white'}>+</RectButton>
-			<RectButton className={'bg-[#FFA600] text-xl font-bold text-white'}>+</RectButton>
-			<RectButton className={'bg-[#EA4335] text-xl font-bold text-white'}>+</RectButton>
+			<RectButton className={'bg-[#4285F4] text-xl font-bold text-white'}>
+				<img src="/image/Group 5877.svg" alt="Left Arrow" width={12} height={12} />
+			</RectButton>
+			<RectButton className={'bg-[#FFA600] text-xl font-bold text-white'}>
+				<img src="/image/Vector-1.svg" alt="Right Arrow" width={12} height={12} />
+			</RectButton>
+			<RectButton className={'bg-[#EA4335] text-xl font-bold text-white'}>
+				<img src="/image/Vector.svg" alt="Up Arrow" width={12} height={12} />
+			</RectButton>
 		</div>
 	);
 }
