658217c9dca13715d399d5e3e253114bd99fa7f8
haidar <haidar.syaafie@gmail.com>
2025-06-05 01:49:11 +0700

    Fixing Hero

--- src/components/organisms/Hero.js
+++ src/components/organisms/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Hero() {
     return (
-        <div className="flex flex-col items-center justify-center text-center bg-[url('/image/hero.png')] bg-cover bg-top w-screen h-[75vh] bg-no-repeat">
+        <div className="flex flex-col items-center justify-center text-center bg-[url('/image/hero.png')] bg-cover bg-bottom w-screen h-[75vh] bg-no-repeat">
             <h1 className="text-4xl  text-[#2C2D3E] mb-4 font-[var(--font-pt-serif)]">
                 WELCOME TO OUR WEBSITE
             </h1>
