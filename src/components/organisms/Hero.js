03844ce930121ab618bf26f73f20066d7f09517f
haidar <haidar.syaafie@gmail.com>
2025-06-05 02:17:12 +0700

    Fixing Hero

--- src/components/organisms/Hero.js
+++ src/components/organisms/Hero.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { PT_Serif } from "next/font/google";
 
+const ptSerif = PT_Serif({
+    // variable: "--font-pt-serif",
+    subsets: ["latin"],
+    weight: "700",
+    style: "italic"
+});
 
 export default function Hero() {
     return (
-        <div className="flex flex-col items-center justify-center text-center bg-[url('/image/hero.png')] bg-cover bg-bottom w-screen h-[75vh] bg-no-repeat">
-            <h1 className={`text-4xl  text-[#ffffff] mb-4  font-extrabold`}>
-                WELCOME TO OUR WEBSITE
+        <div className="flex flex-col items-center justify-center text-center bg-[url('/image/hero.png')] bg-cover bg-bottom w-screen h-[70vh] bg-no-repeat">
+            <h1 className={`text-[#ffffff]  ${ptSerif.className} leading-normal text-7xl`}>
+                "Membaca Buku,<br/>Menjalajahi Dunia"
             </h1>
         </div>
     );
