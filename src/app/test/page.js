206234b4cdb7eab95c5e38cf627b5e411e5d0921
haidar <haidar.syaafie@gmail.com>
2025-06-05 00:11:22 +0700

    Adding Navbar

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,14 +1,10 @@
-import NavigateUtils from '@/components/molecules/NavigateUtils';
-import LeftUtils from '@/components/molecules/LeftUtils';
-import RowUtils from '@/components/molecules/RowUtils';
+import Navbar from '@/components/organisms/Navbar';
 import React from 'react';
 
 export default function Page() {
 	return (
-		<div className="flex items-center flex-row gap-[10px] p-4">
-			<NavigateUtils />
-			<LeftUtils />
-			<RowUtils />
+		<div className="flex items-center flex-row gap-[10px]">
+			<Navbar />
 		</div>
 	);
 }
