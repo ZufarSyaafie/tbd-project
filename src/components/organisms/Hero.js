4d32b62eab61f2e77e6c1e12cefbfe1357d9289f
ZufarSyaafie <zufar.syaafie@gmail.com>
2025-06-13 19:44:58 +0700

    change image hero

--- src/components/organisms/Hero.js
+++ src/components/organisms/Hero.js
@@ -10,7 +10,7 @@ const ptSerif = PT_Serif({
 
 export default function Hero({ heroImage, heroText1 }) {
 	return (
-		<div className="flex h-[50vh] w-screen flex-col items-center justify-center bg-[url('/image/hero.png')] bg-cover bg-bottom bg-no-repeat text-center md:h-[75vh]">
+		<div className="flex h-[50vh] w-screen flex-col items-center justify-center bg-[url('/image/hero.webp')] bg-cover bg-bottom bg-no-repeat text-center md:h-[75vh]">
 			<h1
 				className={`text-[#ffffff] ${ptSerif.className} text-5xl leading-normal md:text-7xl`}
 			>
